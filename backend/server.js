80e2fdf8c04521c759d78f6a8e10611eb8409542
Rdj23 <rohan.jadhav@clevertap.com>
2026-01-08 13:14:43 +0530

    domain

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://csd-sigma.vercel.app"],
+    origin: ["http://localhost:5173", "https://clevertapintel.globalsupportteam.com"],
     credentials: true,
   })
 );
